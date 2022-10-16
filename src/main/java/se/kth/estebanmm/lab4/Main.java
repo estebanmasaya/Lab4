f801bbf0f32fc23c84ad7b4c6f2af4ec1ae88a20
estebanmasaya <estebanmasaya@gmail.com>
2022-10-16 20:43:11 +0200

    fix

--- src/main/java/se/kth/estebanmm/lab4/Main.java
+++ src/main/java/se/kth/estebanmm/lab4/Main.java
@@ -7,8 +7,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import se.kth.estebanmm.lab4.model.Board;
+import se.kth.estebanmm.lab4.model.FileIO;
 import se.kth.estebanmm.lab4.model.SudokuUtilities;
 import se.kth.estebanmm.lab4.view.GridView;
 import se.kth.estebanmm.lab4.view.SudokuView;
@@ -17,6 +19,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        FileChooser fileChooser = new FileChooser();
+        FileIO fileIO = new FileIO(fileChooser);
         Board model = new Board(SudokuUtilities.SudokuLevel.EASY);
         primaryStage.setTitle("Sudoku");
         SudokuView root = new SudokuView(model);
---
+++ src/main/java/se/kth/estebanmm/lab4/model/FileIO.java
@@ -0,0 +1,14 @@
+package se.kth.estebanmm.lab4.model;
+
+import javafx.application.Application;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class FileIO{
+
+    private FileChooser fileChooser;
+
+    public static 
+
+
+}
